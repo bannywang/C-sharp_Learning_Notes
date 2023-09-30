ccecf7408e4af847c654395befc7a384f2f9d378
bannywang <ezra.ishayahu@cloudapps.org.tw>
2023-10-01 00:30:22 +0800

    update

--- hello.py
+++ hello.py
@@ -1,14 +1,14 @@
 a = 200
 b = 20
 
-# if a > b :
+# 流程控制 --------------------------------------------------------
 #     print('A比B大')
 # elif a < b :
 #     print('A比B小')
 # else :
 #     print('一樣大')
 
-
+# 迴圈 --------------------------------------------------------
 # for i in range(100):
 #     print(i)
 
@@ -17,3 +17,23 @@ b = 20
 # while c < 10:
 #     print(c)
 #     c += 1
+
+# 型別 --------------------------------------------------------
+# iv = 10
+# fv = 12.3
+# cv = 3 + 5j
+# sv = 'hello python'
+# bv = True
+# nv = None
+
+# print(type(iv))
+# print(type(fv))
+# print(type(cv))
+# print(type(sv))
+# print(type(bv))
+# print(nv)
+# print(isinstance(sv, str))
+
+# User Input --------------------------------------------------------
+# name = input('Hello, what is your name?  ')
+# print('Hi, ', name)
